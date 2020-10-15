ab24052dc9c5fc07b323522b7411a8200390dfa2
diviinities <71028707+diviinities@users.noreply.github.com>
2020-10-14 23:03:32 -0400

    i made sound in a game yeehaw

---
+++ Assets/PlayerAudio.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAudio : MonoBehaviour
+{
+
+    public AudioClip splashSound;
+
+    public AudioSource audioS;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Water"))
+        {
+            audioS.PlayOneShot(splashSound);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Water"))
+        {
+            audioS.PlayOneShot(splashSound);
+        }
+    }
+
+}
